461ac7269cb95e84736e2f0bd548794a24715c88
Denis <bezbana1@gmail.com>
2020-12-07 17:05:17 +0300

    Add proxy

--- Widget/Api.js
+++ Widget/Api.js
@@ -1,10 +1,12 @@
 const russianNewsApi = 'https://newsapi.org/v2/top-headlines?country=ru&apiKey=efbb712ca15d41b8b7f57df9f8fdf4c8';
-const req = new Request(russianNewsApi);
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const req = new Request(proxyUrl + russianNewsApi);
 
 const newsApi = {
     async getNews() {
         try {
             const response = await fetch(req);
+            console.log(response)
             const data = response.json();
             return data;
         } catch (error) {
